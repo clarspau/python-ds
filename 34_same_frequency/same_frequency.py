71c4a7ef2800ba0c23238e78787a5428f9cc7990
clarspau <125636195+clarspau@users.noreply.github.com>
2023-08-22 22:44:10 -0700

    initial commit

---
+++ 34_same_frequency/same_frequency.py
@@ -0,0 +1,25 @@
+def frequency_counter(freq):
+    """Returns frequency counter mapping of freq."""
+
+    counts = {}
+
+    for x in freq:
+        counts[x] = counts.get(x, 0) + 1
+
+    return counts
+
+
+def same_frequency(num1, num2):
+    """Do these nums have same frequencies of digits?
+
+        >>> same_frequency(551122, 221515)
+        True
+
+        >>> same_frequency(321142, 3212215)
+        False
+
+        >>> same_frequency(1212, 2211)
+        True
+    """
+
+    return frequency_counter(str(num1)) == frequency_counter((str(num2)))
